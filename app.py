aacf8649c9e35bb0aa9640545c3b161a76ebca37
chunhui1110 <98502752+chunhui1110@users.noreply.github.com>
2026-01-09 16:52:54 +0800

    revise

--- app.py
+++ app.py
@@ -3,9 +3,9 @@ import urllib
 import json
 
 app = Flask(__name__)
-@app.route('/<name)')
+@app.route('/<name>')
 def hello(name):
     return "Hello,"+ name + ", How are you!"
 
-if __name__== "__main__":
+if __name__=="__main__":
     app.run()
